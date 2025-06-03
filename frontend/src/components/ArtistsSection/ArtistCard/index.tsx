8f9956c14fbb5c642c2fb75f900c7c2ea7cbb134
Daniel Alexsandro Abrão <abraodaniel@hotmail.com>
2025-06-03 08:01:27 -0300

    adjusting artists section

--- frontend/src/components/ArtistsSection/ArtistCard/index.tsx
+++ frontend/src/components/ArtistsSection/ArtistCard/index.tsx
@@ -1,14 +1,16 @@
 import { IArtistCard } from "@/types/sectionTypes"
 import { Card, Col, Row } from "antd"
 import Image from "next/image"
+import { useState } from "react"
 
 export default function ArtistCard({name, thumb, musicGender, albuns}: IArtistCard) {
+  const [hoverImage, setHoverImage] = useState(false)
   return (
     <Card style={{backgroundColor: 'transparent', border: 'none', width: 250, height: 350, color: 'white'}}>
       <div className="artist-card">
         <Row justify={"center"} style={{textAlign: 'center'}}>
-          <Col span={24}>
-            <Image className="album-thumb" src={thumb} alt="" height={200} width={200}/>
+          <Col span={24} >
+            <Image className={`artist-thumb ${hoverImage ? 'hovered' : ''}`} onMouseEnter={() => setHoverImage(true)} onMouseLeave={() => setHoverImage(false)} src={thumb} alt="" height={200} width={200}/>
           </Col>
           <Col span={24}>
             <p style={{fontWeight: 600, textTransform: 'capitalize'}}>{name}</p>
