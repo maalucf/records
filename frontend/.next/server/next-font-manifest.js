bf27bb9cd04a06f13ebcdaeea4629baedd29bd6c
Daniel Alexsandro Abrão <abraodaniel@hotmail.com>
2025-06-02 21:44:15 -0300

    remove .next from git

--- frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/danielabrao/Documentos/UDESC/records/frontend/src/app/layout\":[\"static/media/569ce4b8f30dc480-s.p.woff2\",\"static/media/93f479601ee12b01-s.p.woff2\",\"static/media/b55f51ff6d79ba97-s.p.woff2\",\"static/media/38778b5ca6981df1-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
