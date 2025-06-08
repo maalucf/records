b0112539dc44fb70677098415fe0acda30936cc4
Daniel Alexsandro Abrão <abraodaniel@hotmail.com>
2025-06-08 20:21:39 -0300

    removing unused content

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,6 +4,7 @@ import AlbumsSection from "@/components/AlbumsSection";
 import ArtistsSection from "@/components/ArtistsSection";
 import MainSection from "@/components/MainSection";
 import SoundWaves from "@/components/SoundWaves";
+import '@ant-design/v5-patch-for-react-19';
 
 export default function Home() {
  
